99044cbfd4c200d157096482c4374027f7d7b01c
Luiz Guilherme Paro <lg_paro@hotmail.com>
2016-08-03 07:38:34 -0300

    Setting up the database configuration

---
+++ src/main/java/br/com/devmedia/appfinal/config/DatabaseConfig.java
@@ -0,0 +1,51 @@
+package br.com.devmedia.appfinal.config;
+
+import javax.sql.DataSource;
+
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+@Configuration
+@ComponentScan("br.com.devmedia.appfinal")
+@PropertySource("classpath:datasource.properties")
+public class DatabaseConfig {
+
+    @Value("${datasource.url}")
+    private String url;
+    
+    @Value("${datasource.username}")
+    private String username;
+    
+    @Value("${datasource.password}")
+    private String password;
+    
+    @Value("${datasource.driverClassName}")
+    private String driverClassName;
+    
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer propertySource() {
+        return new PropertySourcesPlaceholderConfigurer();
+    }
+    
+    @Bean
+    public DataSource dataSource() {
+        BasicDataSource dataSource = new BasicDataSource();
+        dataSource.setUrl(this.url);
+        dataSource.setUsername(this.username);
+        dataSource.setPassword(this.password);
+        dataSource.setDriverClassName(this.driverClassName);
+        
+        return dataSource;
+    }
+    
+    @Bean
+    public JdbcTemplate jdbcTemplate() {
+        return new JdbcTemplate(this.dataSource());
+    }
+}
