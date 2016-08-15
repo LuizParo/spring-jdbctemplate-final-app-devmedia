1db85c11937780c70deb737624f8b36575927335
Luiz Guilherme Paro <lg_paro@hotmail.com>
2016-08-15 09:00:50 -0300

    Fixing the encoding configuration to accept UTF-8 as default charset

--- src/main/java/br/com/devmedia/appfinal/config/WebXmlConfig.java
+++ src/main/java/br/com/devmedia/appfinal/config/WebXmlConfig.java
@@ -1,11 +1,13 @@
 package br.com.devmedia.appfinal.config;
 
+import javax.servlet.FilterRegistration;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRegistration;
 
 import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.servlet.DispatcherServlet;
 
 public class WebXmlConfig implements WebApplicationInitializer {
@@ -19,5 +21,10 @@ public class WebXmlConfig implements WebApplicationInitializer {
         ServletRegistration.Dynamic dynamic = context.addServlet("dispatcher", new DispatcherServlet(webContext));
         dynamic.setLoadOnStartup(1);
         dynamic.addMapping("/");
+        
+        FilterRegistration.Dynamic encodingFilter = context.addFilter("encodingFilter", new CharacterEncodingFilter());
+        encodingFilter.setInitParameter("encoding", "UTF-8");
+        encodingFilter.setInitParameter("forceEncoding", "true");
+        encodingFilter.addMappingForUrlPatterns(null, true, "/*");
     }
 }
